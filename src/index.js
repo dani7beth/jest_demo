174a0a40f098d028ff0c69cd1762e0b990477d8e
dani7beth <danielledona28@gmail.com>
2020-12-02 12:34:20 -0700

    added a new function and test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 export const TEST_STRING = "test string";
+export const testFunction = () => {
+  return TEST_STRING;
+};
+export const sum = (num1, num2) => {
+  return num1 + num2;
+};
+export const minus = (num1, num2) => {
+  return num1 - num2;
+};
+export const divide = (num, den) => {
+  return num / den;
+};
 
-export function testFunction() {
-  return TEST_STRING; 
-  
-}
+export const addPeriodatEnd = (arrayofStrings)=>{
+  //return new array with . added on each string
+  let arrStringPeriodatEnd = arrayofStrings.map((string)=>{
+    return string + '.';
+  });
+  return arrStringPeriodatEnd;
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-import { testFunction, TEST_STRING } from "../src/index";
+import { testFunction, TEST_STRING, sum , minus, divide, addPeriodatEnd} from "../src";
 
 it("returns test string", () => {
   const expected = TEST_STRING;
@@ -7,9 +7,37 @@ it("returns test string", () => {
   expect(result).toBe(expected);
 });
 
-it('adds 1 and 2', ()=>{
-  let num = 1;
-  let num2 = 2;
-  let sum = num + num2;
-  expect(sum).toBe(3);
+// it('adds 1 and 1', ()=>{
+//   let num = 1;
+//   let num2 = 2;
+//   let sum = num + num2;
+//   expect(sum).toBe(3);
+// });
+
+describe('add tests', ()=>{
+
+  test('adds 1 and 1', ()=>{
+    let value = sum(1,1);
+    expect(value).toBe(2);
+  });
+  
+  test('adds 2 and 2', ()=>{
+    let value = sum(2,2);
+    expect(value).toBe(4);
+  });
+ 
+});
+
+describe("it test divide function", ()=>{
+  test("basic divide test", ()=>{
+    let value = divide(4,2);
+    expect(value).toBe(2);
+  });
+});
+
+describe("it test addPeriodatEnd fucntion", ()=>{
+  test("adding a period at end", ()=>{
+    let value = addPeriodatEnd(["string1", "string2", "string3"]);
+    expect(value).toEqual(["string1.", "string2.","string3."]);
+  });
 });
